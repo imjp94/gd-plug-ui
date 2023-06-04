1ac66058f9a52b7f0d4b01e2bdc72c74d3d16cfa
Tan Jian Ping <imjp_94@hotmail.com>
2023-06-04 23:22:09 +0800

    Add to the right of "Plugins" tab

--- addons/gd-plug-ui/plugin.gd
+++ addons/gd-plug-ui/plugin.gd
@@ -8,7 +8,15 @@ var control = UI.instance()
 func _enter_tree():
 	add_control_to_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_LEFT, control)
 	var tab_container = control.get_parent()
-	tab_container.move_child(control, tab_container.get_child_count()-1)
+	var plugins_tab
+	for child in tab_container.get_children():
+		if child.name == "Plugins":
+			plugins_tab = child
+			break
+	if plugins_tab:
+		tab_container.move_child(control, plugins_tab.get_index())
+	else:
+		tab_container.move_child(control, tab_container.get_child_count()-1)
 
 func _exit_tree():
 	remove_control_from_container(EditorPlugin.CONTAINER_PROJECT_SETTING_TAB_LEFT, control)
